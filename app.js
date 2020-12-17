b7d3bf913b202ec0582c2ca48c5964e4dd918320
Zach Smith <65558154+zsmith-dfw@users.noreply.github.com>
2020-12-17 11:40:53 -0600

    configured to use mysql db

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const morgan = require("morgan");
 const routes = require("./routes");
 const { sequelize } = require("./models");
 const cors = require('cors')
+const db = require('./models')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// setup sequelize instance
+const sequelize = new Sequelize('qvyabyz53jrf2nrh', 'e3rh5509go67hfu9', 'lxix2x2peeypu9x9', {
+  host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  dialect: 'mysql'
+})
+
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+  db.sequelize.sync();
+});
 
 // variable to enable global error logging
 const enableGlobalErrorLogging =
@@ -61,9 +75,7 @@ app.use((err, req, res, next) => {
 
 app.set("port", process.env.PORT || 80);
 
-const server = app.listen(app.get("port"), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+
 
 
 
